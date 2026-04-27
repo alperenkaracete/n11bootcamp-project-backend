d659aa2588816e83ebd09da998fc5bccab1f9804
alperenkaracete <m.alperenk@gmail.com>
2026-04-27 21:03:03 +0300

    [+]Initial project setup
    - Mono-repo structure initialized
    - Discovery Server (Eureka) added and configured
    - Config Server added and configured
    - API Gateway (Reactive) added and configured
    - .gitignore updated

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# CHANGELOG
+
+## [0.0.1] - 2026-04-27
+
+### Added
+- Discovery Server (Eureka) added and configured - port 8761
+- Config Server added and configured - port 8888
+- API Gateway (Reactive) added and configured - port 8080
+- Mono-repo structure initialized
+- .gitignore created
---
+++ config-server/src/main/java/n11bootcamp_project_backend/config_server/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package n11bootcamp_project_backend.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
